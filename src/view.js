5f89bd1b1248a2871f8cc06ec2da089a78bc9b70
Igor Zashchelkin <igorzash@tutanota.com>
2022-01-06 18:10:39 +0200

    Archive feature

--- src/view.js
+++ src/view.js
@@ -3,22 +3,66 @@ import storage from "./storage";
 const notesRoot = document.getElementById("notes__root");
 const archievedNotesRoot = document.getElementById("archieved-notes__root");
 
-function renderNote(note, archieved = False) {
+const notesTypeSelect = document.getElementById("notes-type__select");
+
+function toggleArchive() {}
+
+function renderNote(note, archieved) {
 	const note_container = document.createElement("div");
 	note_container.classList.add("note-container");
 
-	const contents = [note.contents, note.date.toLocaleDateString("en-US"), note.category];
+	const contents = [
+		note.contents,
+		note.date.toLocaleDateString("en-US"),
+		note.category,
+	];
 
-	for (let i = 0; i < contents.length; i++) {
+	for (const column_contents of contents) {
 		const column = document.createElement("div");
-		column.textContent = contents[i];
+		column.textContent = column_contents;
 		note_container.appendChild(column);
 	}
 
-	(archieved === true ? archievedNotesRoot : notesRoot).appendChild(note_container);
+	const actions = [
+		{
+			name: "toggleArchive",
+			handler: ({ target }) => {
+				target.parentElement.parentElement.remove();
+				renderNote(note, !archieved);
+			},
+		},
+	];
+
+	const actions_root = document.createElement("div");
+
+	for (const action of actions) {
+		const btn = document.createElement("button");
+		btn.textContent = action.name;
+		btn.addEventListener("click", action.handler);
+
+		actions_root.appendChild(btn);
+	}
+
+	note_container.appendChild(actions_root);
+
+	(archieved === true ? archievedNotesRoot : notesRoot).appendChild(
+		note_container
+	);
 }
 
 export function init() {
-	storage.data.notes.forEach(renderNote);
+	storage.data.notes.forEach((note) => renderNote(note, false));
 	storage.data.archievedNotes.forEach((note) => renderNote(note, true));
+
+	notesTypeSelect.addEventListener('change', (event) => {
+		const index = event.target.selectedIndex;
+
+		if (index === 0) {
+			notesRoot.classList.remove('hidden');
+			archievedNotesRoot.classList.add('hidden');
+		} else if (index === 1) {
+			notesRoot.classList.add('hidden');
+			archievedNotesRoot.classList.remove('hidden');
+		}
+	});
 }
