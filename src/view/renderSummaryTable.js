0db39400d57841a47ac2efb481712d306cdcaf78
Igor Zashchelkin <igorzash@tutanota.com>
2022-01-07 21:29:25 +0200

    Prettier

--- src/view/renderSummaryTable.js
+++ src/view/renderSummaryTable.js
@@ -4,23 +4,23 @@ import summaryTableCalc from "../summaryTable";
 const summaryTable = document.getElementById("summary__root");
 
 function renderSummaryTable() {
-    while (summaryTable.children.length > 1) {
-        summaryTable.lastElementChild.remove();
-    }
+	while (summaryTable.children.length > 1) {
+		summaryTable.lastElementChild.remove();
+	}
 
-    const rows = summaryTableCalc(storage.data.notes);
+	const rows = summaryTableCalc(storage.data.notes);
 
-    rows.forEach(row => {
-        const row_elem = document.createElement('div');
+	rows.forEach((row) => {
+		const row_elem = document.createElement("div");
 
-        row.forEach((column) => {
-            const column_elem = document.createElement('div');
-            column_elem.textContent = column;
-            row_elem.appendChild(column_elem);
-        });
+		row.forEach((column) => {
+			const column_elem = document.createElement("div");
+			column_elem.textContent = column;
+			row_elem.appendChild(column_elem);
+		});
 
-        summaryTable.appendChild(row_elem)
-    });
+		summaryTable.appendChild(row_elem);
+	});
 }
 
 export default renderSummaryTable;
